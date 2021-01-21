d508d8d0aecd7ca0df001261d6b9b3b3bcd267af
Siddhant Mulajkar <mulajkarsid@gmail.com>
2021-01-21 18:15:19 +0530

    modified the app.js and routes.js for proper user redirection and will be working on auth listener in next one

--- src/app.js
+++ src/app.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages';
 import * as ROUTES from './constants/routes';
-
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export default function App() {
-  return( 
-  <Router>
-    <Route exact path="/browse">
-      <Browse />
-    </Route>
-    <Route exact path="/signin">
-      <Signin />
-    </Route>
-    <Route exact path="/signup">
-      <Signup />
-    </Route>
+  const user = null;
+  
+  return ( 
+    <Router>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} 
+          path={ROUTES.SIGN_IN}
+          >
+        <Signin />
+        </IsUserRedirect>
 
-    <Route exact path={ROUTES.HOME}>
-      <Home />
-    </Route>
-  </Router>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} 
+            path={ROUTES.SIGN_UP}
+            >
+          <Signup />
+          </IsUserRedirect>
+        
+        <ProtectedRoute user={user} path={ROUTES.BROWSE} >
+          <Browse />
+        </ProtectedRoute>
+
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} 
+          path={ROUTES.HOME} exact>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
+
+    </Router>
   );
 }
--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,3 +1,52 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
-//timestamp 4:16:32 will work on user fingerprinting
+export function  IsUserRedirect({ user, loggedInPath, children, ...rest}) {
+    return (
+        <Route
+            {...rest}
+            render={() => {
+                if (!user) {
+                    //console.log('user', user);
+                    return children;
+                }
+
+                if (user) {
+                    return (
+                        <Redirect to={{
+                            pathname: loggedInPath,
+                        }}
+                        />
+                    );
+                }
+
+                return null;
+            }}
+        />
+    );
+}
+
+export function ProtectedRoute({ user, children, ...rest }) {
+    return (
+        <Route 
+            {...rest}
+            render={({ location }) => {
+                if(user) {
+                    return children;
+                }
+
+                if(!user) {
+                    return (
+                        <Redirect to={{
+                            pathname: 'signin',
+                            state: { from: location },
+                        }}
+                        />
+                    );
+                }
+
+                return null;
+            }}
+        />
+    );
+}
